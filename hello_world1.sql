7edf4434b4926bc5c17e437f609d2adf418ec30f
Penguin2k4 <168830470+Penguin2k4@users.noreply.github.com>
2025-04-06 20:12:15 +0700

    Add files via upload

---
+++ hello_world1.sql
@@ -0,0 +1,121 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.30 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for my_store
+CREATE DATABASE IF NOT EXISTS `my_store` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `my_store`;
+
+-- Dumping structure for table my_store.account
+CREATE TABLE IF NOT EXISTS `account` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('user','admin') DEFAULT 'user',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table my_store.account: ~1 rows (approximately)
+INSERT INTO `account` (`id`, `username`, `password`, `role`) VALUES
+	(1, 'admin1', '$2y$10$i3TdvImJ80bsdwAJ/hR.y.A6jPo3Rw4smdN50lI0zNw662BvVlhu.', 'admin');
+
+-- Dumping structure for table my_store.ban
+CREATE TABLE IF NOT EXISTS `ban` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `so_ban` int NOT NULL,
+  `trang_thai` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `so_ban` (`so_ban`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table my_store.ban: ~3 rows (approximately)
+INSERT INTO `ban` (`id`, `so_ban`, `trang_thai`) VALUES
+	(1, 1, 'Trống'),
+	(2, 2, 'Đang phục vụ'),
+	(3, 3, 'Đã đặt');
+
+-- Dumping structure for table my_store.category
+CREATE TABLE IF NOT EXISTS `category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table my_store.category: ~2 rows (approximately)
+INSERT INTO `category` (`id`, `name`, `description`) VALUES
+	(1, 'Thức ăn ', NULL),
+	(2, ' Đồ uống', NULL);
+
+-- Dumping structure for table my_store.orders
+CREATE TABLE IF NOT EXISTS `orders` (
+  `order_id` int NOT NULL AUTO_INCREMENT,
+  `table_number` varchar(255) DEFAULT NULL,
+  `order_date` datetime DEFAULT NULL,
+  `total_amount` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table my_store.orders: ~2 rows (approximately)
+INSERT INTO `orders` (`order_id`, `table_number`, `order_date`, `total_amount`) VALUES
+	(1, '2', '2025-03-15 19:15:00', 20000.00),
+	(2, '2', '2025-03-15 19:17:13', 15000.00),
+	(3, '2', '2025-03-15 19:23:47', 40000.00);
+
+-- Dumping structure for table my_store.order_items
+CREATE TABLE IF NOT EXISTS `order_items` (
+  `order_item_id` int NOT NULL AUTO_INCREMENT,
+  `order_id` int DEFAULT NULL,
+  `product_id` int DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`order_item_id`),
+  KEY `order_id` (`order_id`),
+  CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table my_store.order_items: ~2 rows (approximately)
+INSERT INTO `order_items` (`order_item_id`, `order_id`, `product_id`, `quantity`, `price`) VALUES
+	(1, 1, 15, 1, 20000.00),
+	(2, 2, 14, 1, 15000.00),
+	(3, 3, 15, 2, 20000.00);
+
+-- Dumping structure for table my_store.product
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` text,
+  `price` decimal(10,2) NOT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `category_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table my_store.product: ~4 rows (approximately)
+INSERT INTO `product` (`id`, `name`, `description`, `price`, `image`, `category_id`) VALUES
+	(14, 'cafe đá', 'ngon', 15000.00, 'uploads/shopping.png', 2),
+	(15, 'cá viên', 'ngon', 20000.00, 'uploads/ca-vien-chien.jpg', 1),
+	(16, 'nước cam', 'ngon ', 20000.00, 'uploads/tải xuống (1).png', 2),
+	(17, 'bugger', 'ngon', 25000.00, 'uploads/tải xuống.jpg', 1);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
